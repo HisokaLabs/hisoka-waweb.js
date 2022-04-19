a0971914f3d14967cd8c8557e39a13dac59a6378
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2022-04-19 22:38:54 +0700

    :v

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -10,7 +10,7 @@ module.exports = {
     type: "owner",
     start: async (hisoka, m, opt) => {
         let evaled
-        let { name: "Dika Ardnt.", body, from, isMedia, type, sender, prefix, command, commands, args, isOwner, text, quoted, mime, isGroup, metadata, groupName, participants, groupAdmins, isBotAdmin, isAdmin, toUpper, Function } = opt
+        let { name, body, from, isMedia, type, sender, prefix, command, commands, args, isOwner, text, quoted, mime, isGroup, metadata, groupName, participants, groupAdmins, isBotAdmin, isAdmin, toUpper, Function } = opt
         try {
             if (text.endsWith("--sync")) {
                 evaled = await eval(`(async () => { ${text.trim.replace("--sync", "")} })`)
